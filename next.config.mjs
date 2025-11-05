7605e04cfdd7bbc733b81fb33061900d33dfae87
Lucas Racoci <racoci@motorola.com>
2025-11-05 17:14:16 -0300

    Initial setup

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import createMDX from '@next/mdx';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+};
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: ['remark-math'],
+    rehypePlugins: ['rehype-katex'],
+  },
+});
+
+export default withMDX(nextConfig);
